b434731d898b601931a14ceefe4da747feba0532
dean <deanraguso@gmail.com>
2021-06-02 11:51:20 +1000

    Created: JS to implement the auto-updating chatroom

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -11,6 +11,24 @@ consumer.subscriptions.create("RoomChannel", {
   },
 
   received(data) {
+    console.log(data)
+    $('#message_holder').append("<div class='message'>" + data.content + "</div>")
+
     // Called when there's incoming data on the websocket for this channel
   }
 });
+
+$(document).on("turbolinks:load", ()=>{
+  submit_messages();
+})
+
+const submit_messages = () => {
+  $("#message_content").on("keydown", (event)=> {
+    if(event.keyCode === 13){
+      $('input').click()
+      event.target.value = '';
+      event.preventDefault();
+      
+    }
+  })
+}
